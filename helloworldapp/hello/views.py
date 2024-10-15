41367a1374897413ceca30f483f826bb38b3261a
vihar4 <vvihar4@gmail.com>
2024-10-15 11:15:02 -0500

    Add files via upload

---
+++ helloworldapp/hello/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import JsonResponse
+
+def hello_world(request):
+    return JsonResponse({"Message": "Hello World!"})
